39f495801957de104a9e078003b6081851425a8e
calopsitaanderson <calopsitaanderson@gmail.com>
2019-09-04 20:52:02 -0300

    Exercicio 3

---
+++ exercicio3-4.js
@@ -0,0 +1,9 @@
+var ePar = function (num) {
+    return (num % 2) == 0;
+};
+if (ePar(8)) {
+    console.log("O numero \u00E9 PAR");
+}
+else {
+    console.log("O numero \u00E9 IMPAR");
+}
---
+++ exercicio3-4.ts
@@ -0,0 +1,11 @@
+let ePar = function (num:number):boolean{
+        return(num %2) == 0
+}
+if( ePar (8))
+{
+    console.log(`O numero é PAR`)
+}
+else
+{
+    console.log(`O numero é IMPAR`)
+}
